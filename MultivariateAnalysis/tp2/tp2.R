5641c0f74310d7ea8df284ed67a673d64ca79db0
marianaw <mariana.09.w@gmail.com>
2017-06-30 00:37:06 -0300

    Script for second project. Partially complete.

---
+++ MultivariateAnalysis/tp2/tp2.R
@@ -0,0 +1,60 @@
+# Definimos la condición como un factor:
+
+indice$CONDICIO = as.factor(indice$CONDICIO)
+
+# Seleccionamos las filas que nos interesan:
+
+df = indice[, -(1:2)]
+
+# ------------------------------------------------------------
+# -------------- COMPONENTES PRINCIPALES ---------------------
+# ------------------------------------------------------------
+# Calculamos las componentes principales:
+
+S = var(df);S
+pc = prcomp(S, scale. = TRUE)
+summary(pc)
+
+rotation = pc$rotation
+par(mfrow=c(1,2))
+barplot(rotation[,1]); barplot(rotation[,2]); #barplot(rotation[,3])
+
+# Graficamos las componentes más importantes:
+screeplot(pc)
+biplot(pc)
+biplot(pc, choices = c(1,2))
+
+# Correlación entre las variables originales y las componentes:
+Cpc = diag(pc$sdev)
+Corr = rotation %*% Cpc
+
+# ------------------------------------------------------------
+# Repetimos usando la matriz de correlación. Esta vez usamos otro comando:
+pc_corr = princomp(~AUTOFIN+INMACT+INMPN+LIQACID+PNOCOR+PROPACT+RENTECO+SOLVENC+MAREXP+REXP_INT,
+                   cor = TRUE,
+                   data = df)
+summary(pc_corr)
+
+screeplot(pc_corr)  # La varianza se distribuye de un modo más parejo entre las componentes.
+
+rotation_corr = unclass(pc_corr$loadings)
+
+par(mfrow=c(2,2))
+biplot(pc_corr)
+biplot(pc_corr, choices=c(1,2))
+biplot(pc_corr, choices=c(1,3))
+biplot(pc_corr, choices=c(2,3))
+
+# Correlación entre componentes y variables:
+C_corr=diag(pc_corr$sdev^2)
+Corr_X_PC=rotation_corr%*%C_corr
+
+
+# ------------------------------------------------------------
+# -------------- ANÁLISIS DE CORRESPONDENCIA -----------------
+# ------------------------------------------------------------
+
+
+# ------------------------------------------------------------
+# -------------- ANÁLISIS FACTORIAL --------------------------
+# ------------------------------------------------------------
